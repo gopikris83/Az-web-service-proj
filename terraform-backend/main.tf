30a2a7356e49989faa82aff89c856f6c390d2819
Gopi Krishna <gopikrishna.ravichandran@sas.se>
2023-05-14 11:49:03 +0200

    Uncomment backend

--- terraform-backend/main.tf
+++ terraform-backend/main.tf
@@ -11,26 +11,26 @@ provider "azurerm" {
   features {}
 }
 
-# resource "azurerm_resource_group" "tfstate" {
-#   name     = "aznodejsapp-rg"
-#   location = "West Europe"
-# }
+resource "azurerm_resource_group" "tfstate" {
+  name     = "aznodejsapp-rg"
+  location = "West Europe"
+}
 
-# resource "azurerm_storage_account" "tfstate" {
-#   name                     = "nodejssa202304"
-#   resource_group_name      = azurerm_resource_group.tfstate.name
-#   location                 = azurerm_resource_group.tfstate.location
-#   account_tier             = "Standard"
-#   account_replication_type = "LRS"
-#   allow_nested_items_to_be_public = false
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "nodejssa202304"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_nested_items_to_be_public = false
 
-#   tags = {
-#     environment = "test"
-#   }
-# }
+  tags = {
+    environment = "test"
+  }
+}
 
-# resource "azurerm_storage_container" "tfstate" {
-#   name                  = "tfbackend"
-#   storage_account_name  = azurerm_storage_account.tfstate.name
-#   container_access_type = "private"
-# }
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfbackend"
+  storage_account_name  = azurerm_storage_account.tfstate.name
+  container_access_type = "private"
+}
